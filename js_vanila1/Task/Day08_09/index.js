f2561af97e3abd6d986898f57ea8bb61112aac69
Guest <guest-tfxwgr@h27239.sgb.skir.kr>
2019-12-09 19:32:55 +0900

    done Task9

--- js_vanila1/Task/Day08_09/index.js
+++ js_vanila1/Task/Day08_09/index.js
@@ -9,12 +9,31 @@ const FINISHED_LS = "FINISHED";
 let pendings = [];
 let finisheds = [];
 
+function getID() {
+  return pendings.length + finisheds.length + 1;
+}
 function saveDate() {
   localStorage.setItem(PENDING_LS, JSON.stringify(pendings));
   localStorage.setItem(FINISHED_LS, JSON.stringify(finisheds));
 }
 //프린프 + RAM 상의 데이터 올리기.
-function printFinished(Element) {}
+function printFinished(Element) {
+  finisheds.push(Element);
+  const li = document.createElement("li"),
+    span = document.createElement("span"),
+    btnDel = document.createElement("button"),
+    btnMove = document.createElement("button");
+  li.id = Element.id;
+  span.innerText = Element.text;
+  btnDel.innerText = "X";
+  btnDel.addEventListener("click", handleFinishedDelete);
+  btnMove.innerText = "UP";
+  btnMove.addEventListener("click", handleClickUp);
+  li.appendChild(span);
+  li.appendChild(btnDel);
+  li.appendChild(btnMove);
+  indexFinishedList.appendChild(li);
+}
 function printPending(Element) {
   pendings.push(Element);
   const li = document.createElement("li"),
@@ -29,12 +48,13 @@ function printPending(Element) {
   btnMove.addEventListener("click", handleClickDown);
   li.appendChild(span);
   li.appendChild(btnDel);
+  li.appendChild(btnMove);
   indexPendingList.appendChild(li);
 }
 //폼엔터시 패딩에 추가
 function handleSubmit(event) {
   event.preventDefault();
-  const id = pendings.length + finisheds.length;
+  const id = getID();
   const Element = {
     id: id,
     text: indexJsFromInput.value
@@ -44,18 +64,48 @@ function handleSubmit(event) {
   saveDate();
 }
 function handleClickDown(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+
+  handlePendingDelete(event);
+  printFinished({
+    id: parseInt(li.id),
+    text: li.childNodes[0].innerText
+  });
+  saveDate();
+}
+function handleClickUp(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  handleFinishedDelete(event);
+  printPending({
+    id: parseInt(li.id),
+    text: li.childNodes[0].innerText
+  });
   saveDate();
 }
 //패딩에서만 지워진다.
 function handlePendingDelete(event) {
   const btn = event.target;
-  console.dir(btn);
   const li = btn.parentNode;
   indexPendingList.removeChild(li);
   const clearedArray = pendings.filter(e => {
-    return e.id !== parseInt(li.id);
+    return parseInt(e.id) !== parseInt(li.id);
   });
   pendings = clearedArray;
+  saveDate();
+}
+function handleFinishedDelete(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  indexFinishedList.removeChild(li);
+
+  const clearedArray = finisheds.filter(e => {
+    console.log(typeof e.id, ": ", typeof li.id);
+    return parseInt(e.id) !== parseInt(li.id);
+  });
+  console.log(clearedArray);
+  finisheds = clearedArray;
   saveDate();
 }
 function init() {
